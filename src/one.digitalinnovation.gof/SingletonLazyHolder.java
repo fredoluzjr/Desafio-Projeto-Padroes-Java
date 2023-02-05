9731d703fba673a4738df19915013f5d9dfd1cce
Alfredo Luz Junior <89620134+fredoluzjr@users.noreply.github.com>
2023-02-05 19:16:49 -0300

    Inserindo Singleton no projeto

---
+++ src/one.digitalinnovation.gof/SingletonLazyHolder.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof;
+
+// Singleton "Lazy Holder"
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder {
+        private static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+    
+    public SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+}
